01fc8b89b5fe3f512da4c22721f3f253a6440820
NY <ib310519@gmail.com>
2024-06-18 18:57:13 +0800

    create new Xcode project

---
+++ MockAppStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MockAppStore
+//
+//  Created by NY on 2024/6/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
